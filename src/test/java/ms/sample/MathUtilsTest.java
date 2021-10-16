9b4edcb1f9b4dd8fc1d566b175bfcb58278540bc
shuraif <shuraifpilathara@gmail.com>
2021-10-16 10:17:05 +0530

    New annotation added

--- src/test/java/ms/sample/MathUtilsTest.java
+++ src/test/java/ms/sample/MathUtilsTest.java
@@ -4,18 +4,23 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class MathUtilsTest {
 
-	static MathUtils mathUtils;
+	 MathUtils mathUtils;
 	
 	@BeforeAll
-	public static void init() {
+	public  void init() {
 		mathUtils=new MathUtils();
 	}
 	
 	@Test
+	@DisplayName("Testing add method")//display name annotation is used to specify name to the test method
 	void testAdd() {		
 		int expected=2;
 		int actual=mathUtils.add(1, 1);
@@ -32,14 +37,18 @@ class MathUtilsTest {
 		//fail();
 	}	
 	
-	
-	
-	
-	
 	@Test
 	void testComputeCircleArea() {
 		
 		assertEquals(314.1592653589793, mathUtils.computeCircleArea(10)," Should retun circle area");
+	}
+	
+	@Test
+	@Disabled//disabled annotation is used for skipping the test. This will be useful in test driven development.
+	@DisplayName("Test driven development")
+	void testDisabled() {
+		
+		fail("Failed");
 	}
 	
 	
